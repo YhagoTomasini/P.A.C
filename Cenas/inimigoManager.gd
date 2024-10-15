4bab5ce22d00bc590deac418a61c817389fa4289
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2024-10-14 21:12:24 -0300

    Initial commit

---
+++ Cenas/inimigoManager.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+@export var VELO = 50
+@export var nav_agent: NavigationAgent2D
+
+var alvo= null
+var posicaoInicial = Vector2.ZERO
+
+func _ready():
+	posicaoInicial = self.global_position
+	
+	nav_agent.path_desired_distance = 4
+	nav_agent.target_desired_distance = 4
+	
+func _physics_process(_delta):
+	if nav_agent.is_navigation_finished():
+		return
+		
+	var eixo = to_local(nav_agent.get_next_path_position()).normalized()
+	velocity = eixo*VELO
+	
+	move_and_slide()
+	
+func rePath():
+	if alvo:
+		nav_agent.target_position = alvo.global_position
+		
+	else:
+		nav_agent.target_position = posicaoInicial
+	
+
+func _on_re_time_timeout():
+	rePath()
+
+func _on_area_2d_area_entered(area):
+	alvo = area.owner
+
+func _on_area_2d_area_exited(area):
+	if area.owner == alvo:
+		alvo = null
---
+++ Cenas/prismaControler.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+@export var VELO_MAX = 100
+@export var ACELERACAO = 600
+@export var FRICCAO = 400
+@onready var eixos = Vector2.ZERO
+
+func _physics_process(delta):
+	move(delta)
+	
+func get_input_eixos():
+	eixos = Input.get_vector("esquerda", "direita", "cima", "baixo")
+	eixos.x = int(Input.is_action_pressed("direita")) - int(Input.is_action_pressed("esquerda"))
+	eixos.y = int(Input.is_action_pressed("baixo")) - int(Input.is_action_pressed("cima"))
+	return eixos.normalized()
+	
+func move(delta):
+	eixos = get_input_eixos()
+	
+	if eixos == Vector2.ZERO:
+		aplica_friccao(FRICCAO*delta)
+		
+	else:
+		aplica_movimento(eixos*ACELERACAO*delta)
+		
+	move_and_slide()
+	
+func aplica_friccao(amount):
+	if velocity.length() > amount:
+		velocity -= velocity.normalized()*amount
+	else:
+		velocity = Vector2.ZERO
+		
+func aplica_movimento(accel):
+	velocity += accel
+	velocity = velocity.limit_length(VELO_MAX)
+	
